07b4e9ef3e36e326a2e455b971fd8ea22b099c8d
neb <neb@5c36a391-1ffa-0310-89b9-f5e549eca2aa>
2005-08-31 14:40:51 +0000

    ShowPeople: Show last post

--- trunk/include/functions.php
+++ trunk/include/functions.php
@@ -25,26 +25,26 @@ function ShowFeeds($nick, $url, $burl, $images) {
 
         if($rs = $rss->get($url)) {
                 foreach($rs['items'] as $item) {
-			echo("<div class=\"entry\">
-			<b>$item[pubDate]</b>
-			<br />
-			<a href=\"$burl\" title=\"$nick's Blog\"><b>$nick</b></a> <a href=\"$url\" title=\"$nick's Feed\"><img src=\"$images/btn_rss.gif\"></a> said ...</div>\n");
-                        echo("<br /><br />\n");
-			echo("<span class=\"item\"><a href=\"$item[link]\" title=\"$item[title]\">$item[title]</a></span>\n");
-			echo("<br /><br />\n");
-			if ($item['content:encoded'] == "") {
-                        	echo("$item[description]\n");
-			} else {
-                        	echo($item['content:encoded']);
-			}
-                        echo("\n");
-                        echo("$item[wfw]\n");
+                	echo("<div class=\"entry\">
+						<b>$item[pubDate]</b>
+						<br />
+						<a href=\"$burl\" title=\"$nick's Blog\"><b>$nick</b></a> <a href=\"$url\" title=\"$nick's Feed\"><img src=\"$images/btn_rss.gif\"></a> said ...</div>\n");
+                  echo("<br /><br />\n");
+						echo("<span class=\"item\"><a href=\"$item[link]\" title=\"$item[title]\">$item[title]</a></span>\n");
+						echo("<br /><br />\n");
+						if ($item['content:encoded'] == "") {
+                  	echo("$item[description]\n");
+						} else {
+                     echo($item['content:encoded']);
+						}
+                  echo("\n");
+                  echo("$item[wfw]\n");
                 }
                 if($rs['items_count'] <= 0) {
-			echo("<b>ERROR:</b> Sorry, nothing to view on <a href=\"$burl\" title=\"$nick\"><b>$nick's blog</b></a>.\n");
+						echo("<b>ERROR:</b> Sorry, nothing to view on <a href=\"$burl\" title=\"$nick\"><b>$nick's blog</b></a>.\n");
                 }
         } else {
-		echo("<b>ERROR:</b> I cant read the <a href=\"$burl\" title=\"$nick\"><b>$nick's blog</b></a> <b>RSS</b> file.\n<br />\n");
+					echo("<b>ERROR:</b> I cant read the <a href=\"$burl\" title=\"$nick\"><b>$nick's blog</b></a> <b>RSS</b> file.\n<br />\n");
         }
 }
 
@@ -71,9 +71,16 @@ function ShowFaces($nick, $pic, $burl, $faces, $anonymous) {
 # Wee gonna show the nicks here :D
 #
 function ShowPeople($nick, $url, $burl, $images) {
+	global $rss;
+
 	echo("<a href=\"#$nick\" title=\"$nick's Last Post\">$nick</a>\n");
 	echo("<a href=\"$burl\" title=\"$nick's Blog\">(Blog)</a>\n");
 	echo("<a href=\"$url\" title=\"$nick's Feed\"><img src=\"$images/btn_rss.gif\"></a>\n");
+	if($rs = $rss->get($url)) {
+		foreach($rs['items'] as $item) {
+			echo("<br />");
+			echo("<span class=\"lastentry\"><a href=\"$item[link]\" title=\"$nick's Last Post: $item[title]\">$item[title]</a></span>\n");
+		}
 	echo("<br />");
 }
 
